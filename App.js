3310271af236bba1ced16197eba725cd5d938fbe
dannyp0930 <dannyp0930@gmail.com>
2022-10-06 16:17:58 +0900

    FEAT: seperate work and travel

--- App.js
+++ App.js
@@ -59,11 +59,13 @@ export default function App() {
         style={styles.input}
       />
       <ScrollView>
-        {Object.keys(toDos).map((key) => (
-          <View style={styles.toDo} key={key}>
-            <Text style={styles.toDoText}>{toDos[key].text}</Text>
-          </View>
-        ))}
+        {Object.keys(toDos).map((key) =>
+          toDos[key].work === working ? (
+            <View style={styles.toDo} key={key}>
+              <Text style={styles.toDoText}>{toDos[key].text}</Text>
+            </View>
+          ) : null
+        )}
       </ScrollView>
     </View>
   );
